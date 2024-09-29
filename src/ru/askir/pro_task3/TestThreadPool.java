f8665848d2d9e5bdfd6dbcecc5ad6b2bd08e904c
admin <askir@inbox.ru>
2024-09-29 16:33:43 +0300

    Задача 3
    Задача 3

---
+++ src/ru/askir/pro_task3/TestThreadPool.java
@@ -0,0 +1,50 @@
+package ru.askir.pro_task3;
+
+public class TestThreadPool {
+    public static void main(String[] args) {
+        ThreadPool threadPool = new ThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+            final int w = i;
+            threadPool.execute(()->{
+                try {
+                    System.out.println(String.format("Поток %s стартовал ", w));
+                    Thread.sleep(100 + (int) (5000 * Math.random()));
+                    System.out.println(String.format("Поток %s окончен ", w));
+                }
+                catch (InterruptedException exception) {
+                    System.out.println(String.format("Поток %s прерван ", w));
+                }
+            });
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Завершение работы");
+        threadPool.shutdown();
+
+        final int w = 100;
+
+        try {
+            threadPool.execute(() -> {
+                try {
+                    System.out.println(String.format("Поток %s стартовал ", w));
+                    Thread.sleep(100 + (int) (5000 * Math.random()));
+                    System.out.println(String.format("Поток %s окончен ", w));
+                } catch (InterruptedException exception) {
+                    System.out.println(String.format("Поток %s прерван ", w));
+                }
+            });
+        } catch (IllegalStateException exception) {
+            System.out.println(String.format("Поток %s Ошибка: %s", w, exception.getMessage()));
+        }
+
+        System.out.println("Ожидаем остановки");
+        threadPool.awaitTermination();
+        System.out.println("Пул остановлен");
+    }
+}
---
+++ src/ru/askir/pro_task3/ThreadPool.java
@@ -0,0 +1,97 @@
+package ru.askir.pro_task3;
+
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantLock;
+import static java.lang.Thread.sleep;
+
+public class ThreadPool {
+    private ReentrantLock listLock = new ReentrantLock();
+    private boolean isShutdown;
+    private int threadCount;
+    private Thread schedule;
+    private Thread[] threadArray;
+    private LinkedList<Runnable> taskList;
+
+    public ThreadPool(int threadCount) {
+        this.threadCount = threadCount;
+        this.threadArray = new Thread[threadCount];
+        this.taskList = new LinkedList<>();
+        this.isShutdown = false;
+        createSchedule();
+    }
+
+    private void createSchedule() {
+        schedule = new Thread(() -> {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    listLock.lock();
+                    try {
+                        if (isShutdown && taskList.size() == 0) {
+                            return;
+                        }
+
+                        if (taskList.size() > 0) {
+                            int freeThreadIndex = getFreeThreadIndex();
+                            if (freeThreadIndex >= 0) {
+                                Runnable first = taskList.removeFirst();
+
+                                threadArray[freeThreadIndex] = new Thread(first);
+                                threadArray[freeThreadIndex].start();
+                            }
+                        }
+                    }
+                    finally {
+                        listLock.unlock();
+                    }
+
+                    sleep(100);
+                }
+            } catch(InterruptedException exception) {
+                return;
+            }
+        });
+        schedule.start();
+    }
+
+    private int getFreeThreadIndex() {
+        for (int i = 0; i < threadCount; i++) {
+            if(threadArray[i] == null || !threadArray[i].isAlive())
+                return i;
+        }
+        return -1;
+    }
+
+    public void execute(Runnable runnable){
+        listLock.lock();
+
+        try {
+            if(isShutdown)
+                throw new IllegalStateException("Пул в состоянии завершения");
+
+            int freeThreadIndex = getFreeThreadIndex();
+            if(freeThreadIndex < 0)
+                taskList.add(runnable);
+            else {
+                threadArray[freeThreadIndex] = new Thread(runnable);
+                threadArray[freeThreadIndex].start();
+            }
+        }
+        finally {
+            listLock.unlock();
+        }
+    }
+
+    public void shutdown(){
+        isShutdown = true;
+    }
+
+    public void awaitTermination() {
+        while (!(isShutdown && taskList.size() == 0)) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+}
